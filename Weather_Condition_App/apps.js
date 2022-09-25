9fc81cd5df79d3a9b84ca5c10a235d4ce6f0e0c5
Mihail Tal <mihail.tal85@gmail.com>
2022-09-25 23:18:20 +0300

    js updated

--- Weather_Condition_App/apps.js
+++ Weather_Condition_App/apps.js
@@ -1,34 +1,37 @@
+//variables
 const input = document.querySelector("#city_name")
 const submit = document.querySelector("#submit")
 const cities = document.querySelector(".cities")
-let cityList = []
+let cityList = [] //to get all city name
+//to focus input
 input.focus()
+//click submit button, then all functions start working
 submit.addEventListener("click", () => {
-    document.querySelector("#p").innerText = ""
-    document.body.style.backgroundImage = `url(./img/${Math.floor(Math.random()*9)}.png)`
+    document.querySelector("#p").innerText = "" //to clean previous input
+    document.body.style.backgroundImage = `url(./img/${Math.floor(Math.random()*9)}.png)`   //change background randomly
     if (!cityList.includes(input.value)){
         if ((input.value != null)){
-            fetchdata(input.value)
-            cityList.push(input.value)
-            console.log(cityList)
+            fetchdata(input.value)//fetch data receive input value
+            cityList.push(input.value)//push the input value to the array
+            console.log(cityList)//to check input values in array in the console
         }
         else{
-            document.querySelector("#p").innerText = "Please write a city name you want"
-        }input.value =""
+            document.querySelector("#p").innerText = "Please write a city name you want" //to warn user about empty input
+        }input.value =""//to clean previous input
     }
     else{
         document.querySelector("#p").innerText =  `You have already selected ${input.value[0].toUpperCase()+input.value.slice(1)}. Please select another city`
-        input.value =""
+        input.value =""//to clean previous input
     }
  
 })
-
+//keydown to enter
 input.addEventListener("keydown", (e) => {
     if (e.code == "Enter") {
         submit.click()
     }
 })
-let userData;
+//to fetch data from openweathermap.org
 const fetchdata = (input) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.toLowerCase()}&appid=2270405c001a3dbac979c0604b27d8f3`).then((response) => {
         if (!response.ok) {
@@ -39,13 +42,13 @@ const fetchdata = (input) => {
     }).then((data) => updateData(data)).catch((error) => {
         // document.querySelector("#p").innerHTML += error
         document.querySelector("#p").innerText = "You made a mistake. Please write a city name you want"
-        cities.lastElementChild.remove()
-        cityList.pop()
-        console.log(cityList)
+        cities.lastElementChild.remove()//if an error occurs it removes last element
+        cityList.pop()//if an error occurs it removes last input from input
+        console.log(cityList)//to check input values
     })
 }
 
-
+//update data is a function to check all number values if it reaches 3 child elements, it removes first element
 const updateData = (users) => {
     console.log(users)
     if (cities.childElementCount<3){
@@ -61,10 +64,10 @@ const updateData = (users) => {
    
 
 }
-
+//create a div and card including icon image, name of the city,country info and weather condition
 function createDiv(users){
     const newDiv = document.createElement("div")
-    newDiv.className = "card shadow col-lg-3 col-md-6 col-sm-6 m-2"
+    newDiv.className = "card shadow col-lg-3 col-md-6 m-2"
     cities.appendChild(newDiv)
     newDiv.innerHTML += `   
     <div class="card-body">
@@ -75,7 +78,6 @@ function createDiv(users){
     </div>
 `
 }
-
 
 
 
